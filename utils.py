ec98a4a8735c56f25d2ee1156b5976e3c7423f3b
fukka <36649207+fukka@users.noreply.github.com>
2019-12-06 15:56:42 -0500

    Update utils.py
    adding modified_map_pointcloud_to_image

--- utils.py
+++ utils.py
@@ -12,7 +12,7 @@ import torch
 from torch.utils.data.sampler import Sampler
 import torch.nn as nn
 
-from nuscenes import NuScenesExplorer 
+from nuscenes import * 
 from nuscenes.utils.data_classes import LidarPointCloud, Box
 from nuscenes.utils.geometry_utils import view_points
 
@@ -295,3 +295,80 @@ def visualize_result(anchor_point, offset, gt_boxes):
       render_pcl(gt_boxes[i].T, name = str(i))
    plt.show()
 
+def modified_map_pointcloud_to_image(explorer,
+                                pointsensor_token: str,
+                                camera_token: str,
+                                min_dist: float = 1.0,
+                                render_intensity: bool = False) -> Tuple:
+        """
+        Given a point sensor (lidar/radar) token and camera sample_data token, load point-cloud and map it to the image
+        plane.
+        :param pointsensor_token: Lidar/radar sample_data token.
+        :param camera_token: Camera sample_data token.
+        :param min_dist: Distance from the camera below which points are discarded.
+        :param render_intensity: Whether to render lidar intensity instead of point depth.
+        :return (pointcloud <np.float: 2, n)>, coloring <np.float: n>, image <Image>).
+        """
+        cam = explorer.nusc.get('sample_data', camera_token)
+        pointsensor = explorer.nusc.get('sample_data', pointsensor_token)
+        pcl_path = osp.join(explorer.nusc.dataroot, pointsensor['filename'])
+        if pointsensor['sensor_modality'] == 'lidar':
+            pc = LidarPointCloud.from_file(pcl_path)
+        else:
+            pc = RadarPointCloud.from_file(pcl_path)
+        im = Image.open(osp.join(explorer.nusc.dataroot, cam['filename']))
+
+        # Points live in the point sensor frame. So they need to be transformed via global to the image plane.
+        # First step: transform the point-cloud to the ego vehicle frame for the timestamp of the sweep.
+        cs_record = explorer.nusc.get('calibrated_sensor', pointsensor['calibrated_sensor_token'])
+        pc.rotate(Quaternion(cs_record['rotation']).rotation_matrix)
+        pc.translate(np.array(cs_record['translation']))
+
+        # Second step: transform to the global frame.
+        poserecord = explorer.nusc.get('ego_pose', pointsensor['ego_pose_token'])
+        pc.rotate(Quaternion(poserecord['rotation']).rotation_matrix)
+        pc.translate(np.array(poserecord['translation']))
+
+        # Third step: transform into the ego vehicle frame for the timestamp of the image.
+        poserecord = explorer.nusc.get('ego_pose', cam['ego_pose_token'])
+        pc.translate(-np.array(poserecord['translation']))
+        pc.rotate(Quaternion(poserecord['rotation']).rotation_matrix.T)
+
+        # Fourth step: transform into the camera.
+        cs_record = explorer.nusc.get('calibrated_sensor', cam['calibrated_sensor_token'])
+        pc.translate(-np.array(cs_record['translation']))
+        pc.rotate(Quaternion(cs_record['rotation']).rotation_matrix.T)
+
+        # Fifth step: actually take a "picture" of the point cloud.
+        # Grab the depths (camera frame z axis points away from the camera).
+        depths = pc.points[2, :]
+
+        if render_intensity:
+            assert pointsensor['sensor_modality'] == 'lidar', 'Error: Can only render intensity for lidar!'
+            # Retrieve the color from the intensities.
+            # Performs arbitary scaling to achieve more visually pleasing results.
+            intensities = pc.points[3, :]
+            intensities = (intensities - np.min(intensities)) / (np.max(intensities) - np.min(intensities))
+            intensities = intensities ** 0.1
+            intensities = np.maximum(0, intensities - 0.5)
+            coloring = intensities
+        else:
+            # Retrieve the color from the depth.
+            coloring = depths
+
+        # Take the actual picture (matrix multiplication with camera-matrix + renormalization).
+        points = view_points(pc.points[:3, :], np.array(cs_record['camera_intrinsic']), normalize=False)
+
+        # Remove points that are either outside or behind the camera. Leave a margin of 1 pixel for aesthetic reasons.
+        # Also make sure points are at least 1m in front of the camera to avoid seeing the lidar points on the camera
+        # casing for non-keyframes which are slightly out of sync.
+        mask = np.ones(depths.shape[0], dtype=bool)
+        mask = np.logical_and(mask, depths > min_dist)
+        mask = np.logical_and(mask, points[0, :] > 1)
+        mask = np.logical_and(mask, points[0, :] < im.size[0] - 1)
+        mask = np.logical_and(mask, points[1, :] > 1)
+        mask = np.logical_and(mask, points[1, :] < im.size[1] - 1)
+        points = points[:, mask]
+        coloring = coloring[mask]
+
+        return points, coloring, im
