0e0ea464aa87f3483d16c39569d573c950007c68
fukka <choufuuka@gmail.com>
2019-12-25 00:47:41 +0800

    temp to fix gt depth loss

--- generate_data_locally_temp.py
+++ generate_data_locally_temp.py
@@ -33,6 +33,59 @@ from mpl_toolkits.mplot3d import Axes3D
 import cv2 
 
 
+def get_gt(nusc, corners, camera_token, pointsensor_token):
+    cam = nusc.get('sample_data', camera_token)
+    pointsensor = nusc.get('sample_data', pointsensor_token)
+    pcl_path = os.path.join(nusc.dataroot, pointsensor['filename'])
+    # im = Image.open(os.path.join(nusc.dataroot, cam['filename']))
+    pc = corners
+
+    # Points live in the point sensor frame. So they need to be transformed via global to the image plane.
+    # First step: transform the point-cloud to the ego vehicle frame for the timestamp of the sweep.
+    cs_record = nusc.get('calibrated_sensor', pointsensor['calibrated_sensor_token'])
+    pc.rotate(Quaternion(cs_record['rotation']).rotation_matrix)
+    pc.translate(np.array(cs_record['translation']))
+
+    # Second step: transform to the global frame.
+    poserecord = nusc.get('ego_pose', pointsensor['ego_pose_token'])
+    pc.rotate(Quaternion(poserecord['rotation']).rotation_matrix)
+    pc.translate(np.array(poserecord['translation']))
+
+    # Third step: transform into the ego vehicle frame for the timestamp of the image.
+    poserecord = nusc.get('ego_pose', cam['ego_pose_token'])
+    pc.translate(-np.array(poserecord['translation']))
+    pc.rotate(Quaternion(poserecord['rotation']).rotation_matrix.T)
+
+    # Fourth step: transform into the camera.
+    cs_record = nusc.get('calibrated_sensor', cam['calibrated_sensor_token'])
+    pc.translate(-np.array(cs_record['translation']))
+    pc.rotate(Quaternion(cs_record['rotation']).rotation_matrix.T)
+
+    # Fifth step: actually take a "picture" of the point cloud.
+    # Grab the depths (camera frame z axis points away from the camera).
+    depths = pc.points[2, :]
+
+    # Retrieve the color from the depth.
+    coloring = depths
+
+    # Take the actual picture (matrix multiplication with camera-matrix + renormalization).
+    points = view_points(pc.points[:3, :], np.array(cs_record['camera_intrinsic']), normalize=True)
+
+    # Remove points that are either outside or behind the camera. Leave a margin of 1 pixel for aesthetic reasons.
+    # Also make sure points are at least 1m in front of the camera to avoid seeing the lidar points on the camera
+    # casing for non-keyframes which are slightly out of sync.
+    mask = np.ones(depths.shape[0], dtype=bool)
+    mask = np.logical_and(mask, depths > min_dist)
+    mask = np.logical_and(mask, points[0, :] > 1)
+    mask = np.logical_and(mask, points[0, :] < im.size[0] - 1)
+    mask = np.logical_and(mask, points[1, :] > 1)
+    mask = np.logical_and(mask, points[1, :] < im.size[1] - 1)
+    points = points[:, mask]
+    coloring = coloring[mask]
+
+    return points, coloring, im
+
+
 if __name__ == '__main__':
     data_path = r"/home/fengjia/data/sets/nuscenes"
     # save_path = r"/home/fengjia/data/sets/nuscenes_local/vehicle"
@@ -41,7 +94,7 @@ if __name__ == '__main__':
         print('save path not exist')
         exit(0)
 
-    nusc = NuScenes(version='v1.0-trainval', dataroot=data_path, verbose=True)
+    ç = NuScenes(version='v1.0-trainval', dataroot=data_path, verbose=True)
     explorer = NuScenesExplorer(nusc)
 
     PATH = data_path + '/CAMFRONT.txt'
@@ -69,7 +122,19 @@ if __name__ == '__main__':
             vis_level = int(nusc.get('visibility', visibility_token)['token'])
             if (vis_level != 3) and (vis_level != 4):
                 continue
+
+            sd_record = nusc.get('sample_data', im_token)
+            cs_record = nusc.get('calibrated_sensor', sd_record['calibrated_sensor_token'])
+            sensor_record = nusc.get('sensor', cs_record['sensor_token'])
+            ori_corners_ = get_gt(nusc=nusc, corners=box.corners(), camera_token=sensor_record, pointsensor_token=lidar_token)
+
+
             ori_corners = view_points(box.corners(), view=np.array(img_camera_intrinsic, copy=True), normalize=True)
+
+
+            print(counter, ori_corners, ori_corners_)
+
+
             if not(((ori_corners[0].max() - ori_corners[0].min()) > 64) and (
                     (ori_corners[1].max() - ori_corners[1].min()) > 64)):
                 continue
